39818eaa9f951be529b0e8b844653bf115a8896d
Bryan <bryan.alvaredo95@gmail.com>
2022-04-14 14:39:17 -0300

    mejora sistema pokedex

--- js/animacion.js
+++ js/animacion.js
@@ -2,6 +2,8 @@ const pokedexSuperior = document.getElementById("pokeSuperior");
 const pokedexInferior = document.getElementById("pokeInferior");
 const pokedexPantalla = document.getElementById("pantalla");
 const botonPokedex = document.getElementById("boton");
+const contenidoPantalla = document.getElementById("infoPantalla");
+const textoClicAqui = document.getElementById("textoPantalla");
 
 botonPokedex.onclick = function() {
     pokedexSuperior.style.bottom = 0;
@@ -9,4 +11,7 @@ botonPokedex.onclick = function() {
     pokedexPantalla.style.top = `4rem`;
     pokedexPantalla.style.height = `27rem`;
     botonPokedex.style.display = `none`;
+    contenidoPantalla.style.opacity = `100%`;
+    contenidoPantalla.style.display = `flex`;
+    textoClicAqui.style.opacity = `100%`;
 }
